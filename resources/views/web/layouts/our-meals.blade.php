43ea2096b37d43347798b02d7fa17b6d6add74ea
AliAtefAli <aliatefaligadali@gmail.com>
2021-03-18 11:14:41 +0200

    our meals

--- resources/views/web/layouts/our-meals.blade.php
+++ resources/views/web/layouts/our-meals.blade.php
@@ -2,11 +2,9 @@
     <div class="container">
         <h2 class="header-section wow zoomIn">{{ __('site.Our Meals') }}</h2>
         <div class="owl-carousel owl-theme our-meal-slider">
-                <a lazy="loading" href="" class="item wow fadeInDown" style="background-image: url({{ asset('assets/uploads/meals/0db8626e-dc20-46a3-8de1-ff30c8675f59.png' ) }}"></a>
-                <a lazy="loading" href="" class="item wow fadeInDown" style="background-image: url({{ asset('assets/uploads/meals/0db8626e-dc20-46a3-8de1-ff30c8675f59.png' ) }}"></a>
-                <a lazy="loading" href="" class="item wow fadeInDown" style="background-image: url({{ asset('assets/uploads/meals/0db8626e-dc20-46a3-8de1-ff30c8675f59.png' ) }}"></a>
-                <a lazy="loading" href="" class="item wow fadeInDown" style="background-image: url({{ asset('assets/uploads/meals/0db8626e-dc20-46a3-8de1-ff30c8675f59.png' ) }}"></a>
-                <a lazy="loading" href="" class="item wow fadeInDown" style="background-image: url({{ asset('assets/uploads/meals/0db8626e-dc20-46a3-8de1-ff30c8675f59.png' ) }}"></a>
+            @foreach($our_meals as $meal)
+                <a lazy="loading" href="" class="item wow fadeInDown" style="background-image: url({{ asset('assets/uploads/meals/' . $meal->image ) }}"></a>
+            @endforeach
         </div>
     </div>
 </div>
